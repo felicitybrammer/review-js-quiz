96ada4fb4ae00def57c048a1ce85d280c24dca24
Felicity Brammer <felicitybrammer@gmail.com>
2022-06-21 20:19:53 -0700

    remove start screen

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,17 @@
+var startButtonEl = document.getElementById('start');
+var questionsEl = document.getElementById('questions');
+
+function startQuiz() {
+    console.log('click');
+    var startScreenContainer = document.getElementById('start-screen');
+    startScreenContainer.setAttribute('class', 'hide');
+
+    questionsEl.removeAttribute('class', 'hide');
+    //timer starts
+
+    
 
 
+};
 
+startButtonEl.addEventListener('click', startQuiz);
